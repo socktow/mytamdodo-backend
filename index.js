62845b6250eecf7e4c6b63d866d2de6ce4d11ff9
ChuChu Zihu Ms <mjssdn95@gmail.com>
2024-08-27 23:24:03 +0700

    update

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const Task = require('./models/Task'); // Import your Task model
 const taskRouter = require('./routers/taskRouter'); // Import your router
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000; // Sử dụng PORT từ biến môi trường
 const cors = require('cors');
 app.use(cors());
 // Middleware to parse incoming JSON requests
@@ -14,7 +15,7 @@ app.use(express.json());
 app.use('/api', taskRouter);
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://chuchu1:tuan123@cluster0.o3pv3ft.mongodb.net/todolist', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
